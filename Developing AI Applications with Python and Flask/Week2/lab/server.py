33a3d28fce6b689fbe55a2b073349882d3b79d0b
Rodrigo B. de Alexandre <rodrigodealexandre@gmail.com>
2024-05-13 17:27:11 -0300

    lab 2.1

--- Developing AI Applications with Python and Flask/Week2/lab/server.py
+++ Developing AI Applications with Python and Flask/Week2/lab/server.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "hello world"
+
+@app.route("/no_content")
+def no_content():
+    """return 'no content found' with a status of 204
+    Returns:
+        string: no content found with 204 status code
+    """
+    return({"204":"No content found"}, 204)
+
+@app.route("/exp")
+def index_explicit():
+    """
+    return 'Hello World' message with a status code of 200
+    Returns:
+        string: Hello World
+        status code: 200
+    """
+    resp = make_response({"200":"Hello World"})
+    resp.status_code = 200
+    return resp
