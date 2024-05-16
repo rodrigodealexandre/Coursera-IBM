5675766a81436dcca6d81443f9f8ea4d4de50884
Rodrigo B. de Alexandre <rodrigodealexandre@gmail.com>
2024-05-16 11:06:04 -0300

    week3 lab1 task3

--- Developing AI Applications with Python and Flask/Week3/Practice Project Sentiment Analysis/practice_project/sentiment_analysis.py
+++ Developing AI Applications with Python and Flask/Week3/Practice Project Sentiment Analysis/practice_project/sentiment_analysis.py
@@ -1,8 +1,13 @@
-import requests
+import requests, json
 
-def setiment_analyzer(text_to_analyse):
+def sentiment_analyzer(text_to_analyse):
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     headers = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     myobj =  { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    
+    formatted_response = json.loads(response.text)
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
+    
+    return {"label": label, "score": score}
