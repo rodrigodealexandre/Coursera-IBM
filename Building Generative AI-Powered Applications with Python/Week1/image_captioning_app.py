8639a97b26aa77308c664a4e4dfd52b9d3312f51
Rodrigo B. de Alexandre <rodrigodealexandre@gmail.com>
2024-05-19 09:37:06 -0300

    Week1 image_captioning_app

--- Building Generative AI-Powered Applications with Python/Week1/image_captioning_app.py
+++ Building Generative AI-Powered Applications with Python/Week1/image_captioning_app.py
@@ -4,8 +4,8 @@ from PIL import Image
 from transformers import AutoProcessor, BlipForConditionalGeneration
 
 # Load the pretrained processor and model
-processor = # write your code here
-model = # write your code here
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 # Define the image captioning function
 def caption_image(input_image: np.ndarray):
@@ -13,13 +13,13 @@ def caption_image(input_image: np.ndarray):
     raw_image = Image.fromarray(input_image).convert('RGB')
     
     # Process the image
-    
+    inputs = processor(images=raw_image, return_tensors="pt")
 
     # Generate a caption for the image
-
+    outputs = model.generate(**inputs, max_length=50)
 
     # Decode the generated tokens to text and store it into `caption`
-    
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
 
     return caption
 
