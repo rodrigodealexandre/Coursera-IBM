fe4fba2083064922055c522469331e5820cc5f11
Rodrigo B. de Alexandre <rodrigodealexandre@gmail.com>
2024-06-26 16:55:33 -0300

    end of lab1

--- Django Application Development with SQL and Databases/module_4/lab1_template/onlinecourse/views.py
+++ Django Application Development with SQL and Databases/module_4/lab1_template/onlinecourse/views.py
@@ -7,7 +7,57 @@ from django.views import generic, View
 from django.http import Http404
 
 # Create your class based views here.
+# Note that we are subclassing CourseListView from base View class
+#class CourseListView(View):
+#
+#    # Handles get request
+#    def get(self, request):
+#        context = {}
+#        course_list = Course.objects.order_by('-total_enrollment')[:10]
+#        context['course_list'] = course_list
+#        return render(request, 'onlinecourse/course_list.html', context)
 
+# Note that CourseListView is subclassing from generic.ListView instead of View
+# so that it can use attributes and override methods from ListView such as get_queryset()
+class CourseListView(generic.ListView):
+    template_name = 'onlinecourse/course_list.html'
+    context_object_name = 'course_list'
+
+    # Override get_queryset() to provide list of objects
+    def get_queryset(self):
+       courses = Course.objects.order_by('-total_enrollment')[:10]
+       return courses
+
+
+class EnrollView(View):
+
+    # Handles post request
+    def post(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase total enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
+#class CourseDetailsView(View):
+#
+#    # Handles get request
+#    def get(self, request, *args, **kwargs):
+#        context = {}
+#        # We get URL parameter pk from keyword argument list as course_id
+#        course_id = kwargs.get('pk')
+#        try:
+#            course = Course.objects.get(pk=course_id)
+#            context['course'] = course
+#            return render(request, 'onlinecourse/course_detail.html', context)
+#        except Course.DoesNotExist:
+#            raise Http404("No course matches the given id.")
+
+# Note that CourseDetailsView is now subclassing DetailView 
+class CourseDetailsView(generic.DetailView):
+    model = Course
+    template_name = 'onlinecourse/course_detail.html'
 
 
 # Function based views
